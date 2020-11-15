21d29b700957381126a4264abe8726b5b581feeb
zzHee <772575675@qq.com>
2020-11-15 12:41:16 +0800

    D:/Program Files (x86)/git/register接口返回

--- 后端/challengelog/src/main/java/com/challengelog/controller/UserController.java
+++ 后端/challengelog/src/main/java/com/challengelog/controller/UserController.java
@@ -1,12 +1,14 @@
 package com.challengelog.controller;
 
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.challengelog.bean.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.text.DateFormat;
 import java.util.List;
 import java.util.Map;
 
@@ -19,15 +21,34 @@ public class UserController {
 
     @GetMapping("/userList")
     public List<Map<String,Object>> userList(){
-        //宝贝，这里mysql查询语句别忘记带分号
+        //这里mysql查询语句别忘记带分号
         String sql = "select * from user;";
         List<Map<String,Object>> list_maps = jdbcTemplate.queryForList(sql);
         return list_maps;
     }
 
+    @ResponseBody
     @RequestMapping("/register")
-    public String register() {
-        return "register";
+    public String register(@RequestBody JSONObject jsonParam) {
+        System.out.println(jsonParam.toJSONString());
+        int userid = Integer.parseInt(jsonParam.get("userid").toString());
+        System.out.println(userid);
+        String open_id = "temp";
+        User user = new User(userid, open_id, "nick_name", "none", "男", DateFormat.getInstance());
+        String sql = "insert into user values(" + user.getId() + "," + user.getOpen_id() + ","
+                + user.getNick_name() + "," + user.getAvatar_url() + "," + user.getGender() + ","
+                + user.getCreate_time() + ");";
+        System.out.println(sql);
+
+////        String sql = "insert into user values(3,'open_id','nick_name', 'avatar_url', '男', current_time);";
+//        System.out.println(jdbcTemplate.update(sql));
+//        String sql1 = "select * from challenges;";
+//        List<Map<String,Object>> list_maps = jdbcTemplate.queryForList(sql1);
+////        JSONObject responsesjson = new JSONObject();
+////        String temp = "{'status': true}";
+////        return JSON.toJSONString(temp);
+////        return list_maps.toString();
+        return "none";
     }
 
     @RequestMapping("/login")
