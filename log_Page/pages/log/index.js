5d2f798cd5bcc64c2d44ff491d77cd12f4220012
Chensfv <1641254998@qq.com>
2020-11-17 23:22:20 +0800

    day4_log

--- log_Page/pages/log/index.js
+++ log_Page/pages/log/index.js
@@ -14,56 +14,37 @@ Page({
     }),
     logList:([
       {
-        "user_id":4,
-        "log_id":4,
-        "line_id":4,
-        "time":"2020-11-14",
-        "story":{
-          "story_id":4,
-          "story_title":"当科状元",
-          "story_content":"具体内容为、、、、"
-        }
+        "diary_id":"4",
+        "title":"日记4",
+        "time":"2020-4-4",
+        "content":"日记内容"
       },
       {
-        "user_id":3,
-        "log_id":3,
-        "line_id":3,
-        "time":"2020-11-13",
-        "story":{
-          "story_id":3,
-          "story_title":"科举",
-          "story_content":"具体内容为、、、、"
-        }
+        "diary_id":"3",
+        "title":"日记3",
+        "time":"2020-3-3",
+        "content":"日记内容"
       },
       {
-        "user_id":2,
-        "log_id":2,
-        "line_id":2,
-        "time":"2020-11-12",
-        "story":{
-          "story_id":2,
-          "story_title":"读书与练武",
-          "story_content":"具体内容为、、、、"
-        }
+        "diary_id":"2",
+        "title":"日记2",
+        "time":"2020-2-2",
+        "content":"日记内容"
       },
       {
-        "user_id":1,
-        "log_id":1,
-        "line_id":1,
-        "time":"2020-11-11",
-        "story":{
-          "story_id":1,
-          "story_title":"我出生啦",
-          "story_content":"具体内容为、、、、"
-        }
+        "diary_id":"1",
+        "title":"日记1",
+        "time":"2020-1-1",
+        "content":"日记内容"
       }
     ])
   },
   /*点击事件*/
   btnTap1:function(event){
+    // console.log(event)
     wx.showModal({
       title: '剧情',
-      content:event.target.dataset.content,
+      content:event.currentTarget.dataset.content,
       success (res) {
         if (res.confirm) {
           console.log('用户点击确定')
@@ -82,14 +63,13 @@ Page({
       url: 'api.imix.cn/',
       dataType: "JSON",
       data:{
-        "user_id":1,
-        "line_id":1
+        "user_id":1
       },
       method:"POST",
-      success: (result) => {
+      success: (result) => {log
         this.setData({
-          logList:result.message,
-          main_line:result.log
+          logList:result.log,
+          main_line:result.main_line
         });
       },
       fail: (res) => {
@@ -97,6 +77,7 @@ Page({
       },
       complete: (res) => {},
     })
+    // this.draw_line;
   },
 
   /**
@@ -147,6 +128,14 @@ Page({
   onShareAppMessage: function () {
 
   }
+  // draw_line:function(){
+  //   const ctx = wx.createCanvasContext('myCanvas');
+  //   ctx.moveTo(10, 10);
+  //   ctx.rect(10, 10, 100, 50);
+  //   ctx.lineTo(110, 60);
+  //   ctx.stroke();
+  //   ctx.draw();
+  // }
 
 
 })
