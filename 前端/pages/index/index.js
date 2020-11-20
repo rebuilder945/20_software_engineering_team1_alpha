e35b4b40a7cc049755c121775e97c358fc678323
Chensfv <1641254998@qq.com>
2020-11-20 17:48:29 +0800

    前端

---
+++ 前端/pages/index/index.js
@@ -0,0 +1,250 @@
+var util = require('../../utils/util');
+Page({
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    nowType: 0,
+    searchState: "noneWxss",
+    editState: "noneWxss",
+    addState: "noneWxss",
+    completeState: "journalEdit",
+    Target: [{
+        id:0,
+        title: "第一个目标",
+        content: "第一个目标内容",
+        start_time: "2020年3月",
+        end_time: "2020年4月",
+        icon_type:"cancel",
+      },
+      {
+        id:1,
+        title: "第二个目标",
+        content: "第二个目标内容",
+        start_time: "2020年3月",
+        end_time: "2020年4月",
+        icon_type:"cancel",
+      },
+      {
+        id:2,
+        title: "第三个目标",
+        content: "第三个目标内容",
+        start_time: "2020年3月",
+        end_time: "2020年4月",
+        icon_type:"cancel",
+      },
+      {
+        id:3,
+        title: "第四个目标",
+        content: "第四个目标内容",
+        start_time: "2020年3月",
+        end_time: "2020年4月",
+        icon_type:"cancel",
+      },
+      {
+        id:4,
+        title: "第五个目标",
+        content: "第五个目标内容",
+        start_time: "2020年3月",
+        end_time: "2020年4月",
+        icon_type:"cancel",
+      }
+    ],
+    newTarget: {
+      title: "第五个目标",
+      content: "第五个目标内容",
+      start_time: "2020年3月",
+      end_time: "2020年4月",
+      icon_type:"cancel",
+    },
+    end_date:"2020-11-17",
+    end_time:"00:00"
+  },
+  //控制页面变化
+  changeSearch: function (e) {
+    var type = this.data.nowType;
+    if (type == 0) {
+      this.setData({
+        searchState: "weui-search-bar",
+        nowType: 1,
+      })
+    } else if (type == 1) {
+      this.setData({
+        searchState: "noneWxss",
+        nowType: 0,
+      })
+    } else if (type == 2) {
+      this.setData({
+        addState: "noneWxss",
+        searchState: "weui-search-bar",
+        nowType: 1,
+      })
+    } else {
+      this.setData({
+        editState: "noneWxss",
+        completeState: "journalEdit",
+        searchState: "weui-search-bar",
+        nowType: 1,
+      })
+    }
+  },
+  changeEdit: function (e) {
+    var type = this.data.nowType;
+    if (type == 0) {
+      this.setData({
+        editState: "journalEdit",
+        completeState: "noneWxss",
+        nowType: 3,
+      })
+    } else if (type == 1) {
+      this.setData({
+        searchState: "noneWxss",
+        editState: "journalEdit",
+        completeState: "noneWxss",
+        nowType: 3,
+      })
+    } else if (type == 2) {
+      this.setData({
+        addState: "noneWxss",
+        editState: "journalEdit",
+        completeState: "noneWxss",
+        nowType: 3,
+      })
+    } else {
+      this.setData({
+        editState: "noneWxss",
+        completeState: "journalEdit",
+        nowType: 0,
+      })
+    }
+  },
+  changeAdd: function (e) {
+    var type = this.data.nowType;
+    var text = this.data.addState;
+    if (type == 0) {
+      this.setData({
+        addState: "addBackGround",
+        nowType: 2,
+      })
+    } else if (type == 1) {
+      this.setData({
+        searchState: "noneWxss",
+        addState: "addBackGround",
+        nowType: 2,
+      })
+    } else if (type == 2) {
+      this.setData({
+        addState: "noneWxss",
+        nowType: 0,
+      })
+    } else {
+      this.setData({
+        editState: "noneWxss",
+        addState: "addBackGround",
+        completeState: "journalEdit",
+        nowType: 2,
+      })
+    }
+  },
+
+  //获得新增日志数据
+  GetTargetValueTitle(e) {
+    this.setData({
+      'newTarget.title': e.detail.value,
+    })
+    console.log(this.data.newTarget.title);
+  },
+  GetDateChange: function (e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      end_date: e.detail.value
+    })
+  },
+  GetTimeChange: function (e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      end_time: e.detail.value
+    })
+  },
+  GetTargetValueContent(e){
+    this.setData({
+      'newTarget.content': e.detail.value,
+    })
+  },
+  CreateNewTarget:function(e){
+    this.setData({
+      'newTarget.start_time': util.formatTime(new Date()),
+      'newTarget.end_time':[this.data.end_date]+" "+[this.data.end_time],
+    })
+    console.log(this.data.newTarget.start_time);
+    console.log(this.data.newTarget.end_time);
+    wx.request({
+      url: '/user/target/add ', 
+      data:{
+        user_id:this.data.user_id,
+        Target:this.data.newTarget,
+      },
+      method: "GET",
+      success(res) {
+        console.log(res.data)
+        this.setData({
+          'Target.push':res.data,
+        })
+      },
+      fail(res) {
+        console.log("fail")
+      },
+      complete: function () {
+        console.log("ffff")
+      }
+    })
+  },
+  onLoad: function (options) {
+    wx.request({
+      url: '/user/target/history',
+      data: this.user_id,
+      method: "POST",
+      success(res) {
+        console.log(res.data)
+        var target=this.data.Target;
+        target.splice(0,0,this.data.newTarget);
+        this.setData({
+         Target:target,
+        });
+      },
+      fail(res) {
+        console.log("fail")
+      },
+      complete: function () {
+        console.log("ffff")
+      }
+    })
+  },
+  // icon_change:function(e){
+  //   var content = e.currentTarget.dataset.challenge;
+  //   wx.showModal({
+  //     title: content.title,
+  //     content:content.content,
+  //     cancelText:"关闭",
+  //     confirmText:"完成挑战",
+  //     success: function(res) {
+  //       if (res.confirm) {
+  //         content.icon_type = "success";
+  //       } else if (res.cancel) {
+  //         console.log('用户点击取消')
+  //       }
+  //       console.log(content.icon_type)
+  //       var set = 'Target['+content.id+'].icon_type';
+  //       this.setData({
+  //         'set':content.icon_type
+  //       })
+  //     }
+  //   })
+  //   // var set = 'Target['+content.id+'].icon_type';
+  //   // // console.log(set)
+  //   // this.setData({
+  //   //   'set':content.icon_type
+  //   // })
+  //   // console.log(this.data.Target[content.id].icon_type)
+  // }
+})
---
+++ 前端/pages/log/index.js
@@ -0,0 +1,159 @@
+// pages/log/index.js
+var utils = require('../../utils/util.js');
+import { request } from "../../request/index.js";
+import util from "../../utils/util.js";
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+
+  data: {
+    
+    time:"",
+    main_line:({
+      "story_id":1,
+      "cover_url":"../../image/story.png"
+    }),
+    logList:([
+      {
+        "diary_id":"4",
+        "title":"日记4",
+        "time":"2020-4-4",
+        "content":"日记内容"
+      },
+      {
+        "diary_id":"3",
+        "title":"日记3",
+        "time":"2020-3-3",
+        "content":"日记内容"
+      },
+      {
+        "diary_id":"2",
+        "title":"日记2",
+        "time":"2020-2-2",
+        "content":"日记内容"
+      },
+      {
+        "diary_id":"1",
+        "title":"日记1",
+        "time":"2020-1-1",
+        "content":"日记内容"
+      }
+    ])
+  },
+  /*点击事件*/
+  btnTap1:function(event){
+    // console.log(event)
+    wx.showModal({
+      title: '剧情',
+      content:event.currentTarget.dataset.content,
+      success (res) {
+        if (res.confirm) {
+          console.log('用户点击确定')
+        } else if (res.cancel) {
+          console.log('用户点击取消')
+        }
+      }
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    wx.request({
+      url: 'api.imix.cn/',
+      dataType: "JSON",
+      data:{
+        "user_id":1
+      },
+      method:"POST",
+      success: (result) => {
+        this.setData({
+          logList:result,
+        });
+      },
+      fail: (res) => {
+        console.log("无法获得数据")
+      },
+      complete: (res) => {},
+    })
+    wx.request({
+      url: 'api.iminx.cn',
+      data:{
+        "user_id":1
+      },
+      dataType:"JSON",
+      method:"POST",
+      success:(result)=>{
+        main_line:result
+      }
+    })
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onchange: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  },
+  // draw_line:function(){
+  //   const ctx = wx.createCanvasContext('myCanvas');
+  //   ctx.moveTo(10, 10);
+  //   ctx.rect(10, 10, 100, 50);
+  //   ctx.lineTo(110, 60);
+  //   ctx.stroke();
+  //   ctx.draw();
+  // },
+  edit_log:function (event) {
+    // console.log(event)
+    wx.navigateTo({
+      url: 'edit_log?'+ '&title=' + event.currentTarget.dataset.item.title + '&time=' + event.currentTarget.dataset.item.time + '&content=' + event.currentTarget.dataset.item.content + '&diary_id=' + event.currentTarget.dataset.item.diary_id
+    })
+  }
+
+})
